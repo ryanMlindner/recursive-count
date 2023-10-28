0a1d517e92e976154f4aff3e2cb633082239c251
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-10-27 23:19:27 -0600

    solution, explanation

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,10 @@
 function recursiveCount(num = 0) {
   // type your code here
+  console.log(num);
+  num++
+  if (num < 10) {
+    recursiveCount(num);
+  }
 }
 
 if (require.main === module) {
@@ -11,3 +16,8 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/**
+ * very simple recursive loop
+ * just move the iterative stop (the while < 10) into the gate for the recursive call
+ * and youre done
+ */
